b179fa15e1f613ebec595658962f7ba25a0321b6
Orie Chinedu <oriechinedu@gmail.com>
2019-07-19 09:12:57 +0100

    Create actions migration

--- data/migrations/20190719090615_actions.js
+++ data/migrations/20190719090615_actions.js
@@ -1,8 +1,20 @@
-
 exports.up = function(knex) {
-  
+  return knex.schema.createTable("actions", table => {
+    actions.increments();
+    table
+      .integer("project_id")
+      .unsigned()
+      .notNullable();
+    table.string("description", 128).notNullable();
+    table.text("notes").notNullable();
+    table.boolean("completed").defaultTo(false);
+    table
+      .foreign("project_id")
+      .references("id")
+      .inTable("projects");
+  });
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTableIfExists("actions");
 };
