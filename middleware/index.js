0be93b67858da57e64774eda78e6e763f08293c0
Orie Chinedu <oriechinedu@gmail.com>
2019-07-19 09:29:33 +0100

    Create middleware to validate project id

--- middleware/index.js
+++ middleware/index.js
@@ -18,8 +18,28 @@ const addProjectValidator = (req, res, next) => {
   }
 }
 
+async function validateProjectId(req, res, next) {
+  try {
+    const { id } = req.params;
+  if(!id || !Number(id)) {
+    throw new ErrorHandler(400, "invalid post id" )
+  } else {
+    const project = await Project.get(id);
+    if (project) {
+      req.project = project;
+      next()
+    } else {
+      throw new ErrorHandler(404, "Project with the specified ID does not exist")
+    }
+  }
+  } catch (error) {
+    next(error)
+  }
+};
+
 
 module.exports = {
   addProjectValidator,
+  validateProjectId,
   
 }
